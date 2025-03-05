a2ec2ef05b4decf18acd6c8ba4c7cef05ced37ab
Ryan Yovanda <ryanyovanda@gmail.com>
2025-03-05 19:55:17 +0700

    build

--- Dockerfile
+++ Dockerfile
@@ -17,7 +17,7 @@ FROM eclipse-temurin:21-jre-alpine
 WORKDIR /app
 
 # Copy the built jar file from the build stage
-COPY --from=build /app/target/fezz4ubackend-0.0.1-SNAPSHOT.jar app.jar
+COPY --from=build /app/target/airgoda-backend-0.0.1-SNAPSHOT.jar app.jar
 
 # Expose the port the app runs on
 EXPOSE 8080
--- src/main/resources/db/migration/V4__PropertyTable.sql
+++ src/main/resources/db/migration/V4__PropertyTable.sql
@@ -1,110 +1,110 @@
-
-create table public.property_category
-(
-    category_id bigserial primary key,
-    name        varchar(100) not null,
-    description text
-);
-
-create table public.properties
-(
-    property_id bigserial primary key,
-    name        varchar(100) not null,
-    category_id bigint references public.property_category,
-    description text,
-    room_id     varchar(100),
-    tenant_id   bigint references public.users,
-    is_active   boolean default true,
-    is_deleted  boolean default false,
-    created_at  timestamp with time zone default CURRENT_TIMESTAMP not null,
-    updated_at  timestamp with time zone default CURRENT_TIMESTAMP not null
-);
-
-create table public.room_variant
-(
-    room_variant_id bigserial primary key,
-    property_id bigint references public.properties,
-    name varchar(100) not null,
-    description text,
-    max_guest integer not null,
-    price numeric(15,2) not null,
-    created_at timestamp with time zone default CURRENT_TIMESTAMP not null,
-    updated_at timestamp with time zone default CURRENT_TIMESTAMP not null
-);
-
-create table public.room_availability
-(
-    availability_id bigserial primary key,
-    room_variant_id bigint references public.room_variant,
-    available_date date not null,
-    is_available boolean default true not null,
-    created_at timestamp with time zone default CURRENT_TIMESTAMP not null,
-    updated_at timestamp with time zone default CURRENT_TIMESTAMP not null
-);
-
-create table public.peak_rate
-(
-    peak_rate_id bigserial primary key,
-    room_variant_id bigint references public.room_variant,
-    start_date date not null,
-    end_date date not null,
-    additional_price numeric(15,2) not null,
-    created_at timestamp with time zone default CURRENT_TIMESTAMP not null,
-    updated_at timestamp with time zone default CURRENT_TIMESTAMP not null
-);
-
-create table public.order_status
-(
-    order_status_id serial primary key,
-    name varchar(100) not null,
-    created_at timestamp default CURRENT_TIMESTAMP not null,
-    updated_at timestamp default CURRENT_TIMESTAMP not null,
-    deleted_at timestamp
-);
-
-create table public.order
-(
-    order_id bigserial primary key,
-    user_id bigint references public.users,
-    total_price numeric(15,2) not null,
-    is_paid boolean default false,
-    created_at timestamp default CURRENT_TIMESTAMP not null,
-    updated_at timestamp default CURRENT_TIMESTAMP not null,
-    deleted_at timestamp
-);
-
-create table public.order_items
-(
-    order_item_id bigserial primary key,
-    order_id bigint references public.order,
-    room_variant_id bigint references public.room_variant,
-    start_date date not null,
-    end_date date not null,
-    total_price numeric(15,2) not null,
-    guest integer not null,
-    created_at timestamp default CURRENT_TIMESTAMP not null,
-    updated_at timestamp default CURRENT_TIMESTAMP not null,
-    deleted_at timestamp
-);
-
-create table public.review
-(
-    review_id bigserial primary key,
-    order_id bigint references public.order,
-    rating integer not null,
-    comment text,
-    created_at timestamp with time zone default CURRENT_TIMESTAMP not null
-);
-
-create table public.discount
-(
-    discount_id bigserial primary key,
-    property_id bigint references public.properties,
-    room_variant_id bigint references public.room_variant,
-    start_date date not null,
-    end_date date not null,
-    discount_type varchar(50) not null,
-    value numeric(15,2) not null,
-    created_at timestamp with time zone default CURRENT_TIMESTAMP not null,
-    updated_at timestamp with time zone default CURRENT_TIMESTAMP not null
-);
+--
+-- create table public.property_category
+-- (
+--     category_id bigserial primary key,
+--     name        varchar(100) not null,
+--     description text
+-- );
+--
+-- create table public.properties
+-- (
+--     property_id bigserial primary key,
+--     name        varchar(100) not null,
+--     category_id bigint references public.property_category,
+--     description text,
+--     room_id     varchar(100),
+--     tenant_id   bigint references public.users,
+--     is_active   boolean default true,
+--     is_deleted  boolean default false,
+--     created_at  timestamp with time zone default CURRENT_TIMESTAMP not null,
+--     updated_at  timestamp with time zone default CURRENT_TIMESTAMP not null
+-- );
+--
+-- create table public.room_variant
+-- (
+--     room_variant_id bigserial primary key,
+--     property_id bigint references public.properties,
+--     name varchar(100) not null,
+--     description text,
+--     max_guest integer not null,
+--     price numeric(15,2) not null,
+--     created_at timestamp with time zone default CURRENT_TIMESTAMP not null,
+--     updated_at timestamp with time zone default CURRENT_TIMESTAMP not null
+-- );
+--
+-- create table public.room_availability
+-- (
+--     availability_id bigserial primary key,
+--     room_variant_id bigint references public.room_variant,
+--     available_date date not null,
+--     is_available boolean default true not null,
+--     created_at timestamp with time zone default CURRENT_TIMESTAMP not null,
+--     updated_at timestamp with time zone default CURRENT_TIMESTAMP not null
+-- );
+--
+-- create table public.peak_rate
+-- (
+--     peak_rate_id bigserial primary key,
+--     room_variant_id bigint references public.room_variant,
+--     start_date date not null,
+--     end_date date not null,
+--     additional_price numeric(15,2) not null,
+--     created_at timestamp with time zone default CURRENT_TIMESTAMP not null,
+--     updated_at timestamp with time zone default CURRENT_TIMESTAMP not null
+-- );
+--
+-- create table public.order_status
+-- (
+--     order_status_id serial primary key,
+--     name varchar(100) not null,
+--     created_at timestamp default CURRENT_TIMESTAMP not null,
+--     updated_at timestamp default CURRENT_TIMESTAMP not null,
+--     deleted_at timestamp
+-- );
+--
+-- create table public.order
+-- (
+--     order_id bigserial primary key,
+--     user_id bigint references public.users,
+--     total_price numeric(15,2) not null,
+--     is_paid boolean default false,
+--     created_at timestamp default CURRENT_TIMESTAMP not null,
+--     updated_at timestamp default CURRENT_TIMESTAMP not null,
+--     deleted_at timestamp
+-- );
+--
+-- create table public.order_items
+-- (
+--     order_item_id bigserial primary key,
+--     order_id bigint references public.order,
+--     room_variant_id bigint references public.room_variant,
+--     start_date date not null,
+--     end_date date not null,
+--     total_price numeric(15,2) not null,
+--     guest integer not null,
+--     created_at timestamp default CURRENT_TIMESTAMP not null,
+--     updated_at timestamp default CURRENT_TIMESTAMP not null,
+--     deleted_at timestamp
+-- );
+--
+-- create table public.review
+-- (
+--     review_id bigserial primary key,
+--     order_id bigint references public.order,
+--     rating integer not null,
+--     comment text,
+--     created_at timestamp with time zone default CURRENT_TIMESTAMP not null
+-- );
+--
+-- create table public.discount
+-- (
+--     discount_id bigserial primary key,
+--     property_id bigint references public.properties,
+--     room_variant_id bigint references public.room_variant,
+--     start_date date not null,
+--     end_date date not null,
+--     discount_type varchar(50) not null,
+--     value numeric(15,2) not null,
+--     created_at timestamp with time zone default CURRENT_TIMESTAMP not null,
+--     updated_at timestamp with time zone default CURRENT_TIMESTAMP not null
+-- );
